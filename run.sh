7ca9f8f4eb5e33dec7004ee220cc69279d4d3b58
Derock <derock@derock.dev>
2025-06-13 16:17:59 +0800

    fix: load home-manager session variables

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash 
 
+source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+
 export RULE_DBUS='quickshell.dbus.properties.warning = false;quickshell.dbus.dbusmenu.warning = false'  # System tray dbus property errors
 export RULE_NOTIFS='quickshell.service.notifications.warning = false'  # Notification server warnings on reload
 export RULE_SNI='quickshell.service.sni.host.warning = false'  # StatusNotifierItem warnings on reload
